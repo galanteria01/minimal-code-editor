15460a3073617cc8e2ea48ba8486d2c88a7f8637
Shoury Sharma <shanuu12e@gmail.com>
2020-10-19 21:17:10 +0530

    Basically added save code functionality

--- Javascript/script2.js
+++ Javascript/script2.js
@@ -1,12 +1,27 @@
-function write(){
-    var html = document.getElementById("html");
-	var css = document.getElementById("css");
-    var js = document.getElementById("js");
-    var writable = document.getElementById("save").contentWindow.document
-    document.getElementById("saveIt").onclick = function() { 
-        document.getElementById('save').innerHTML = "<!DOCTYPE html> <html lang> <head></head>"+
-        htmlCode.value+"<style>"+cssCode.value+"</style>"+"<script>" + jsCode.value + "</script>"
-        +"</html>";
-     }
-	
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
 }
+
+// Start file download.
+document.getElementById("saveIt").addEventListener("click", function(){
+    // Generate download of hello.txt file with some content
+    var htmlCode = document.getElementById("html").value;
+	var cssCode = document.getElementById("css").value;
+    var jsCode = document.getElementById("js").value;
+    var text = ("<!DOCTYPE html> <html lang> <head></head>"+
+    htmlCode+"<style>"+cssCode+"</style>"+"<script>" + jsCode + "</script>"
+    +"</html>");
+    
+    var filename = "code.txt";
+    
+    download(filename, text);
+}, false);
